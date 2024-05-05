e4449448a874c4681b5581236ecc04cb09fedc71
Koontungza <tungtung309@gmail.com>
2024-05-06 03:03:28 +0700

    complete the chat function and scroll but not express

--- lib/screens/chat/individual_chat_page.dart
+++ lib/screens/chat/individual_chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 // import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -8,60 +9,84 @@ import 'package:flutter_dating_app/screens/chat/chat_bubbles.dart';
 import 'package:flutter_dating_app/screens/chat/chat_service.dart';
 // import 'package:flutter_dating_app/services/chat_service.dart';
 
-class IndividualChatPage extends StatelessWidget {
+class IndividualChatPage extends StatefulWidget {
   final String receiverEmail;
   final String receiverId;
   IndividualChatPage(
       {super.key, required this.receiverEmail, required this.receiverId});
 
+  @override
+  State<IndividualChatPage> createState() => _IndividualChatPageState();
+}
+
+class _IndividualChatPageState extends State<IndividualChatPage> {
   final ChatServices _chatService = ChatServices();
 
   final messageController = TextEditingController();
+
+  FocusNode myFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    myFocusNode.addListener(() {
+      if (myFocusNode.hasFocus) {
+        Future.delayed(const Duration(milliseconds: 500), () => scrollDown());
+      }
+    });
+
+    Future.delayed(const Duration(milliseconds: 500), () => scrollDown());
+  }
+
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    messageController.dispose();
+    super.dispose();
+  }
+
+  final ScrollController _scrollController = ScrollController();
+  void scrollDown() {
+    _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+        duration: const Duration(seconds: 1), curve: Curves.fastOutSlowIn);
+  }
 
   void sendMessage() async {
     if (messageController.text.isNotEmpty) {
       // print("sendButton Pressed");
 
-      await _chatService.sendMessage(receiverId, messageController.text);
+      await _chatService.sendMessage(widget.receiverId, messageController.text);
       messageController.clear();
+      scrollDown();
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          receiverEmail,
-          style: const TextStyle(
-              color: Color(
-                  0xFFFFFFFF)), // Assuming you want the text color to be 0xFFBB254A
-        ),
-        centerTitle: true,
-        backgroundColor: const Color(0xFFBB254A),
-        leading: const BackButton(color: Colors.white),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0), // Adjust padding as needed
-              child: _buildMessageList(),
-            ),
+        appBar: AppBar(
+          title: Text(
+            widget.receiverEmail,
+            style: const TextStyle(
+                color: Color(
+                    0xFFBB254A)), // Assuming you want the text color to be 0xFFBB254A
           ),
-          Container(
-              height: 80,
-              decoration: const BoxDecoration(color: Color(0xFFBB254A)),
-              child: _buildUserInput()),
-        ],
-      ),
-    );
+          centerTitle: true,
+          backgroundColor: const Color(0xFFFFFFFF),
+          leading: const BackButton(color: Color(0xFFBB254A)),
+        ),
+        body: Column(
+          children: [
+            Expanded(child: _buildMessageList()),
+            _buildUserInput(),
+          ],
+        ));
   }
 
   Widget _buildMessageList() {
     String currentUserID = FirebaseAuth.instance.currentUser!.uid;
     return StreamBuilder(
-        stream: _chatService.getMessages(currentUserID, receiverId),
+        stream: _chatService.getMessages(currentUserID, widget.receiverId),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
             return const Text("ERROR");
@@ -74,6 +99,7 @@ class IndividualChatPage extends StatelessWidget {
           }
 
           return ListView(
+            controller: _scrollController,
             children: snapshot.data!.docs
                 .map((doc) => _buildMessageItem(doc))
                 .toList(),
@@ -123,35 +149,42 @@ class IndividualChatPage extends StatelessWidget {
 
   Widget _buildUserInput() {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              controller: messageController,
-              decoration: InputDecoration(
-                hintText: 'Type a message...',
-                hintStyle: const TextStyle(
-                  fontSize: 14,
-                  fontFamily: 'Sk-Modernist',
-                  fontWeight: FontWeight.w700,
-                  color: Color(0xFFBB254A),
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: SizedBox(
+        height: 80,
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: messageController,
+                decoration: InputDecoration(
+                  hintText: 'Type a message...',
+                  hintStyle: const TextStyle(
+                    fontSize: 14,
+                    fontFamily: 'Sk-Modernist',
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFFBB254A),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 10.0),
                 ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide.none,
-                ),
-                filled: true,
-                fillColor: Colors.grey[200],
-                contentPadding: const EdgeInsets.symmetric(
-                    horizontal: 20.0, vertical: 10.0),
+                focusNode: myFocusNode,
               ),
             ),
-          ),
-          IconButton(
-              onPressed: sendMessage,
-              icon: const Icon(Icons.send, color: Colors.white)),
-        ],
+            IconButton(
+              onPressed: () => {
+                sendMessage(),
+              }, // Arrow function for sequential execution
+              icon: const Icon(Icons.send, color: Color(0xFFBB254A)),
+            ),
+          ],
+        ),
       ),
     );
   }
